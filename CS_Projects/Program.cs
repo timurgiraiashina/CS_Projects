810d2cb83bc509fbf705f3f21fd9aa716a26d7a0
timurgiraiashina <timurgiraiashina@gmail.com>
2022-06-13 14:43:15 +0300

    Add project files.

---
+++ CS_Projects/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Projects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1= 2147483647;
+            Console.WriteLine("Integer veri tipi pozitif max sayı {0}",number1);
+            Console.ReadLine();
+        }
+    }
+}
