0fc778cc75a5f6eedea3df04d0d65a538e2139ba
timurgiraiashina <timurgiraiashina@gmail.com>
2022-06-22 11:00:04 +0300

    Türler ve değişkenler

--- CS_Projects/Program.cs
+++ CS_Projects/Program.cs
@@ -11,8 +11,24 @@ namespace CS_Projects
         static void Main(string[] args)
         {
             int number1= 2147483647;
+            long number2 = 9223372036854775807;
+            short number3 = 32767;
+            byte number4 = 255;
+            bool degil = false;
+            char number5 = 'a';
+            double number6 = 1.7e+308;
+            decimal number7 = 7.9m+28;
+            var degisken = 10;//eğer object ile degisken kelimesini kullansaydık birden fazla değer tutabilirdi
+            //degisken = "degisken"; //hata verir çünkü var ile sadece 1 atama yapılabilir
+
+            Console.WriteLine("long veri tipi max pozitif sayı {0}", number2);
+
             Console.WriteLine("Integer veri tipi pozitif max sayı {0}",number1);
             Console.ReadLine();
         }
+    }
+    enum gunler {
+        Pazartesi,Salı,Çarşamba,Perşembe,Cuma,Cumartesi,Pazar
+    
     }
 }
